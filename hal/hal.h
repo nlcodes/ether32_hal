c92f43ba1c8da5fcafe3ce3ef370336be63d1f3c
Nathan <nluskcodes@gmail.com>
2025-02-08 00:54:47 -0800

    Add hal code for status led drivers including led_on_off and led_blink

--- hal/hal.h
+++ hal/hal.h
@@ -2,6 +2,7 @@
 #define HAL_H
 
 #include "hal_oled.h"
+#include "hal_status_led.h"
 
 /* Include defined in hal.h to avoid linter error */
 void hal_display_init();
---
+++ hal/hal_status_led.h
@@ -0,0 +1,8 @@
+#ifndef HAL_STATUS_LED_H
+#define HAL_STATUS_LED_H
+
+int hal_led_on_off(int state){
+
+}
+
+#endif
