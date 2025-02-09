2414f85450a46295e63f07dbc4cfda68220f81ea
Nathan <nluskcodes@gmail.com>
2025-02-09 00:38:38 -0800

    Fix status_led.h; correct return type and include stdint.h

--- hal/hal_status_led.h
+++ hal/hal_status_led.h
@@ -1,7 +1,9 @@
 #ifndef HAL_STATUS_LED_H
 #define HAL_STATUS_LED_H
 
-int hal_led_on_off(volatile uint8_t running);
+#include <stdint.h>
+
+void hal_led_on_off(volatile uint8_t running);
 
 void hal_led_blink();
 
