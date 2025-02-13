0eb99ffcfe4ea324eedcb8942d5b9c58af5f48e7
Nathan <nluskcodes@gmail.com>
2025-02-12 19:02:13 -0800

    Simplify make.scm

--- make_utils/make.scm
+++ make_utils/make.scm
@@ -1,19 +1,9 @@
-; Build script for gambit scheme on unix systems
+; Build script for gambit scheme lisp on unix systems
 ; Works with swd
+; Use with other make utils in stm32_hal after moving all make utils to master dir of project
 ; After installing gambit scheme, run with "gsi make.scm"
-
-; Define run command function
-(define (run_cmd cmd)
-  (shell-command cmd))
-
-; Define make function
-(define (make)
-
-  ; Runs each command in order
-  (run_cmd "make clean")
-  (run_cmd "make")
-  (run_cmd "arm-none-eabi-objcopy -O binary test.elf test.bin")
-  (run_cmd "st-flash write test.bin 0x8000000"))
-
-; Call make function
-(make)
+ 
+(shell-command 
+  "make clean && make && \
+   arm-none-eabi-objcopy -O binary test.elf test.bin && \
+   st-flash write test.bin 0x8000000")
