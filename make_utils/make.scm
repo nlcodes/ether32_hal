6441f81af775b9fe1116df1b7306bd64833378b2
Nathan <nluskcodes@gmail.com>
2025-03-03 23:35:48 -0800

    Create readme.txt for make_utils; remove usage instructions from make.scm comments

--- make_utils/make.scm
+++ make_utils/make.scm
@@ -1,7 +1,4 @@
-; Build script for gambit scheme lisp on unix systems
-; Works with swd
-; Use with other make utils in stm32_hal after moving all make utils to master dir of project
-; After installing gambit scheme, run with "gsi make.scm"
+; Build script for stm32_hal 
  
 (shell-command 
   "make clean && make && \
